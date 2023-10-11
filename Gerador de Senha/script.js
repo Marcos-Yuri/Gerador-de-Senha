f8971e4be5af8a081ea3c1b88218ef5bd1fc5f57
Marcos Yuri <127909955+Marcos-Yuri@users.noreply.github.com>
2023-10-10 21:58:28 -0300

    Add files via upload

--- Gerador de Senha/script.js
+++ Gerador de Senha/script.js
@@ -1,21 +1,35 @@
 
 let sliderElement = document.querySelector("#slider");
-let buttonElement = document.querySelector("button");
+let buttonElement = document.querySelector("#button");
 
 let sizePassword = document.querySelector("#valor");
-let password = document.querySelector("password");
+let password = document.querySelector("#password");
 
-let conteinerPassword = document.querySelector("container-password");
+let conteinerPassword = document.querySelector("#container-password");
 
-let charset ="abcdefghijklmnopqrstuvxwyzABCDEFGHIJKLMNOPQRSTUVXWYZ0123456789";
+let charset ="abcdefghijklmnopqrstuvxwyzABCDEFGHIJKLMNOPQRSTUVXWYZ0123456789!@";
 let novaSenha = "";
 
 sizePassword.innerHTML = sliderElement.value;
 
-slider.oniput = function(){
+slider.oninput = function(){
     sizePassword.innerHTML = this.value;
 }
 
 function generatePassword(){
-    alert("VAMOS GERAR A SENHA")
+
+    let pass = "";
+    for(let i = 0, n = charset.length; i < sliderElement.value; ++i){
+        pass += charset.charAt(Math.floor(Math.random() * n));
+    }
+
+    conteinerPassword.classList.remove("hide");
+    password.innerHTML = pass;
+    novaSenha = pass;
+
+}
+
+function copyPassword(){
+    alert("Senha copiada com sucesso!")
+    Navigator.clipboard.writeText(novaSenha);
 }
